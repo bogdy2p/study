b4fb5d39b422ad739500b85eae0dedf425965071
Popa Bogdan <bogdy2p@yahoo.com>
2015-03-17 14:24:49 +0200

    Created the ADD method

--- src/Pbc/Bundle/NotificationBundle/Controller/NotificationController.php
+++ src/Pbc/Bundle/NotificationBundle/Controller/NotificationController.php
@@ -2,8 +2,34 @@
 
 namespace Pbc\Bundle\NotificationBundle\Controller;
 
-class NotificationController 
-{
-    
-        
+class NotificationController {
+
+    private $defaults = array(
+                "type" => "flash",
+                    ),
+            $flashes = array();
+
+    /**
+     * Depending on the argument , add the values to the session flashBag or $this->flashes
+     * 
+     * @param string $name
+     * @param array $arguments
+     */
+    public function add($name, array $arguments = array()) {
+
+        $arguments += $this->defaults;
+        // If the type is flash then add the values to the session flashBag
+        if ($arguments["type"] === "flash") {
+            $this->session->getFlashBag()->add($name, $arguments);
+        }
+        // Otherwise (is instant) then add them to the class variable $flashes
+        elseif ($arguments["type"] === "instant") {
+            //We want to be able to have multiple notifications of the same name i.e "success" so we need to ad each new sset of arguments into an array not overwrite the last one
+            if (!isset($this->flashes[$name])) {
+                $this->flashes[$name] = array();
+            }
+            $this->flashes[$name][] = $arguments;
+        }
+    }
+
 }
