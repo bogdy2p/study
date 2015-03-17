58652ad2cc7859b6ed2cd99f6a4a64b112bb2dc1
Popa Bogdan <bogdy2p@yahoo.com>
2015-03-17 15:44:34 +0200

    Created the render solo and multiple methods

--- src/Pbc/Bundle/NotificationBundle/Twig/NotificationExtension.php
+++ src/Pbc/Bundle/NotificationBundle/Twig/NotificationExtension.php
@@ -9,15 +9,39 @@ class NotificationExtension extends \Twig_Extension {
     protected $container, $notify;
 
     public function __construct(ContainerInterface $container = null) {
-        
+
         $this->container = $container;
         $this->notify = $container->get("pbc.notify");
     }
 
+    public function renderAll($container = false) {
 
+        $notifications_array = $this->notify->all();
 
+        if (count($notifications_array) > 0) {
+            return $this->container->get('templating')
+                            ->render(
+                                    "PbcNotificationBundle:Notification:multiple.html.twig", compact("notifications_array", "container")
+            );
+        }
+        return null;
+    }
 
+    public function renderOne($name, $container = false) {
 
+        if (!$this->notify->has($name)) {
+            return false;
+        }
+        $notifications = $this->notify->get($name);
+
+        if (count($notifications) > 0) {
+            return $this->container->get('templating')
+                            ->render(
+                                    "PbcNotificationBundle:Notification:single.html.twig", compact("notifications" . "container")
+            );
+        }
+        return null;
+    }
 
     public function getName() {
 
