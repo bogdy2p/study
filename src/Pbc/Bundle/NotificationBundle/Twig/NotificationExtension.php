fa46576bbd3ce0819f799353f2710ec637650855
Popa Bogdan <bogdy2p@yahoo.com>
2015-03-17 15:29:12 +0200

    Twig extension part

---
+++ src/Pbc/Bundle/NotificationBundle/Twig/NotificationExtension.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pbc\Bundle\NotificationBundle\Twig;
+
+class NotificationExtension extends \Twig_Extension {
+
+    public function getName() {
+
+        return 'pbc_notification_extension';
+    }
+
+}
