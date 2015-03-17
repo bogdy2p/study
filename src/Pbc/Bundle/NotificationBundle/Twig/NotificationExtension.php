56884a96b997be5ed59a6c657c5c81f816e6b849
Popa Bogdan <bogdy2p@yahoo.com>
2015-03-17 15:32:38 +0200

    Get the two services neede for render functions

--- src/Pbc/Bundle/NotificationBundle/Twig/NotificationExtension.php
+++ src/Pbc/Bundle/NotificationBundle/Twig/NotificationExtension.php
@@ -2,7 +2,22 @@
 
 namespace Pbc\Bundle\NotificationBundle\Twig;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class NotificationExtension extends \Twig_Extension {
+
+    protected $container, $notify;
+
+    public function __construct(ContainerInterface $container = null) {
+        
+        $this->container = $container;
+        $this->notify = $container->get("pbc.notify");
+    }
+
+
+
+
+
 
     public function getName() {
 
