21aa3ead2b5b61d3f833c419b7e61ce5c790f127
Popa Bogdan <bogdy2p@yahoo.com>
2015-03-18 10:26:24 +0200

    Implemented printing 4 notifications

--- src/Pbc/Bundle/NotificationBundle/Controller/DefaultController.php
+++ src/Pbc/Bundle/NotificationBundle/Controller/DefaultController.php
@@ -16,6 +16,11 @@ class DefaultController extends Controller
     {
         $notify = $this->get("pbc.notify");
         $notify->add("test", array("type" => "instant", "message" => "this is awesome"));
+        $notify->add("asd", array("type" => "instant", "message" => "this is not awesome"));
+        $notify->add("asd2", array("type" => "instant", "message" => "Lolozaur :) "));
+        $notify->add("asd3", array("type" => "instant", "message" => "~AwEsOmE~"));
+        
+        
         
         if($notify->has("test")) {
             return array("notifications" => $notify->get("test"));
